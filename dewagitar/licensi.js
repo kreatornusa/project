bda2fb7c39c6eef3127c78149bd1509879b4adff
Kreator Nusantara <93833445+kreatornusa@users.noreply.github.com>
2022-01-11 19:49:36 +0700

    Update licensi.js

--- dewagitar/licensi.js
+++ dewagitar/licensi.js
@@ -1,1 +1,1 @@
-[{\"id\":1,\"url\":\"dagruel.com\"}, {\"id\":2,\"url\":\"news.dagruel.com\"}, {\"id\":3,\"url\":\"forum.dagruel.com\"}]
+[{\"id\":1,\"url\":\"lirik.dewagitar.com\"}, {\"id\":2,\"url\":\"news.dagruel.com\"}, {\"id\":3,\"url\":\"forum.dagruel.com\"}]
